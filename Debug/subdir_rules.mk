6b0eabbff85f395778a335cebe092fb0c2b9a52c
stawro <stawro159@gmail.com>
2019-09-02 13:40:12 +0200

    Initial Code

---
+++ Debug/subdir_rules.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+BLMGR.obj: ../BLMGR.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"c:/ti/ccsv6/tools/compiler/arm_5.1.6/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.6/include" --check_misra="all,-1" --misra_required=error --misra_advisory=error -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="BLMGR.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+BLMGR_CFG.obj: ../BLMGR_CFG.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"c:/ti/ccsv6/tools/compiler/arm_5.1.6/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.6/include" --check_misra="all,-1" --misra_required=error --misra_advisory=error -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="BLMGR_CFG.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+BLTD.obj: ../BLTD.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"c:/ti/ccsv6/tools/compiler/arm_5.1.6/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.6/include" --check_misra="all,-1" --misra_required=error --misra_advisory=error -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="BLTD.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+CRC.obj: ../CRC.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"c:/ti/ccsv6/tools/compiler/arm_5.1.6/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.6/include" --check_misra="all,-1" --misra_required=error --misra_advisory=error -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="CRC.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+KEYPAD.obj: ../KEYPAD.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"c:/ti/ccsv6/tools/compiler/arm_5.1.6/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.6/include" --check_misra="all,-1" --misra_required=error --misra_advisory=error -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="KEYPAD.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+L3G4200D.obj: ../L3G4200D.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"c:/ti/ccsv6/tools/compiler/arm_5.1.6/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.6/include" --check_misra="all,-1" --misra_required=error --misra_advisory=error -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="L3G4200D.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+L3G4200D_Lcfg.obj: ../L3G4200D_Lcfg.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"c:/ti/ccsv6/tools/compiler/arm_5.1.6/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.6/include" --check_misra="all,-1" --misra_required=error --misra_advisory=error -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="L3G4200D_Lcfg.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+uart_test.obj: ../uart_test.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"c:/ti/ccsv6/tools/compiler/arm_5.1.6/bin/armcl" -mv4 --code_state=32 --abi=ti_arm9_abi --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.6/include" --check_misra="all,-1" --misra_required=error --misra_advisory=error -g --display_error_number --diag_warning=225 --diag_wrap=off --preproc_with_compile --preproc_dependency="uart_test.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,66 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../BLMGR.c \
+../BLMGR_CFG.c \
+../BLTD.c \
+../CRC.c \
+../KEYPAD.c \
+../L3G4200D.c \
+../L3G4200D_Lcfg.c \
+../uart_test.c 
+
+OBJS += \
+./BLMGR.obj \
+./BLMGR_CFG.obj \
+./BLTD.obj \
+./CRC.obj \
+./KEYPAD.obj \
+./L3G4200D.obj \
+./L3G4200D_Lcfg.obj \
+./uart_test.obj 
+
+C_DEPS += \
+./BLMGR.pp \
+./BLMGR_CFG.pp \
+./BLTD.pp \
+./CRC.pp \
+./KEYPAD.pp \
+./L3G4200D.pp \
+./L3G4200D_Lcfg.pp \
+./uart_test.pp 
+
+C_DEPS__QUOTED += \
+"BLMGR.pp" \
+"BLMGR_CFG.pp" \
+"BLTD.pp" \
+"CRC.pp" \
+"KEYPAD.pp" \
+"L3G4200D.pp" \
+"L3G4200D_Lcfg.pp" \
+"uart_test.pp" 
+
+OBJS__QUOTED += \
+"BLMGR.obj" \
+"BLMGR_CFG.obj" \
+"BLTD.obj" \
+"CRC.obj" \
+"KEYPAD.obj" \
+"L3G4200D.obj" \
+"L3G4200D_Lcfg.obj" \
+"uart_test.obj" 
+
+C_SRCS__QUOTED += \
+"../BLMGR.c" \
+"../BLMGR_CFG.c" \
+"../BLTD.c" \
+"../CRC.c" \
+"../KEYPAD.c" \
+"../L3G4200D.c" \
+"../L3G4200D_Lcfg.c" \
+"../uart_test.c" 
+
+
